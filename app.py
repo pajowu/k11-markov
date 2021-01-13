d45bab407d93dd4a0a209d4d0ea1803a1eeca1b0
pajowu <pajowu@pajowu.de>
2021-01-13 10:35:11 +0100

    ✨ Add more series

--- app.py
+++ app.py
@@ -1,14 +1,77 @@
 from flask import Flask
-from model import model
+from models import MODELS
 import random
+import json
+
 app = Flask(__name__)
 
-with open("template.html") as f:
-	TEMPLATE = f.read()
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-@app.route('/')
-def hello_world():
-	sentence = model.make_sentence(tries=10000) or "Fehler! Bitte neuladen"
-	season = str(random.randint(1, 15))
-	episode = str(random.randint(1, 100))
-	return TEMPLATE.replace("%% SENTENCE %%",sentence).replace("%% EPISODE %%",episode).replace("%% SEASON %%", season)
+env = Environment(
+    loader=FileSystemLoader("./templates"),
+    autoescape=select_autoescape(["html", "xml"]),
+)
+
+with open("data/cities.json") as f:
+    CITIES = json.load(f)
+with open("data/animals.json") as f:
+    ANIMALS = json.load(f)
+
+MODEL_NAMES = [(id, model["name"]) for id, model in MODELS.items()]
+MODEL_NAMES.sort(key=lambda x: x[1])
+
+
+@app.route("/")
+def index():
+    # sentence = model.make_sentence(tries=10000) or "Fehler! Bitte neuladen"
+    # season = str(random.randint(1, 15))
+    # episode = str(random.randint(1, 100))
+    # return TEMPLATE.replace("%% SENTENCE %%",sentence).replace("%% EPISODE %%",episode).replace("%% SEASON %%", season)
+    return str(MODELS.keys())
+
+
+@app.route("/soko")
+def soko():
+    model = MODELS["soko"]
+    template = env.get_template("soko.html")
+    city = random.choice(CITIES)
+    title = model["model"].make_sentence(tries=10000) or "Fehler! Bitte neuladen"
+    return template.render(
+        {"title": title, "city": city, "model": model, "models": MODEL_NAMES}
+    )
+
+
+@app.route("/tierpark")
+def tierpark():
+    model = MODELS["tierpark"]
+    title = model["model"].make_sentence(tries=10000) or "Fehler! Bitte neuladen"
+    animal1 = random.choice(ANIMALS)
+    animal2 = random.choice(ANIMALS)
+    template = env.get_template("tierpark.html")
+    return template.render(
+        {
+            "title": title,
+            "animal1": animal1,
+            "animal2": animal2,
+            "model": model,
+            "models": MODEL_NAMES,
+        }
+    )
+
+
+@app.route("/<model_name>")
+def model(model_name):
+    model = MODELS[model_name]
+    title = model["model"].make_sentence(tries=10000) or "Fehler! Bitte neuladen"
+    season = str(random.randint(1, 15))
+    episode = str(random.randint(1, 100))
+    template = env.get_template("generic_model.html")
+    return template.render(
+        {
+            "title": title,
+            "season": season,
+            "episode": episode,
+            "model": model,
+            "models": MODEL_NAMES,
+        }
+    )
--- generate_models.py
+++ generate_models.py
@@ -8,26 +8,29 @@ import argparse
 INPUT_PATH = Path("data/input")
 OUTPUT_PATH = Path("data/model")
 
-def main(state_size):
-	INPUT_PATH.mkdir(exist_ok=True)
-	OUTPUT_PATH.mkdir(exist_ok=True)
+def main(state_size, models):
+    INPUT_PATH.mkdir(exist_ok=True)
+    OUTPUT_PATH.mkdir(exist_ok=True)
 
-	for file in INPUT_PATH.glob("*.json"):
-	    print(f"Generating model for {file.name}")
-	    with open(file) as f:
-	        input = json.load(f)
+    for file in INPUT_PATH.glob("*.json"):
+        if models and file.stem not in models:
+            continue
+        print(f"Generating model for {file.name}")
+        with open(file) as f:
+            input = json.load(f)
 
-	    text_model = markovify.Text(input["names"], state_size=state_size)
+        text_model = markovify.Text(input["names"], state_size=state_size)
 
-	    output_dict = input
-	    output_dict['model'] = text_model.to_dict()
-	    with open(OUTPUT_PATH / file.name, "w") as f:
-	        json.dump(output_dict, f, indent=4)
+        output_dict = input
+        output_dict['model'] = text_model.to_dict()
+        with open(OUTPUT_PATH / file.name, "w") as f:
+            json.dump(output_dict, f, indent=4)
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--state-size", type=int, default=1)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--state-size", type=int, default=1)
+    parser.add_argument("--model_name", action="append")
+    args = parser.parse_args()
 
-	main(args.state_size)
+    main(args.state_size, args.model_name)
